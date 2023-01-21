cc76a52c97ce134689c4e73ccc603cb5d13dbaed
stephen.chukwurah <stephen.chukwurah@cecureintel.com>
2023-01-21 20:23:06 +0100

    base href set

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/real-world-react-app-basic">
       <App />
     </BrowserRouter>
   </React.StrictMode>
